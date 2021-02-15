d23e67f0b2af3373ebf693062af09fd4e06e0716
Polyana L. Pimenta <polyana.pimenta@gmail.com>
2021-02-15 15:52:57 -0300

    fixing CustomButton type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -21,7 +21,7 @@ class SignIn extends React.Component {
     e.preventDefault();
 
     const { email, password } = this.state;
-    
+
     if (!email || !password) {
       alert('Please enter your credentials');
       return;
@@ -67,7 +67,7 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
           </div>
